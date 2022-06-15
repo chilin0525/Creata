11fcf3ca87274332e520589925cf49b0f17959a9
Chilin Ubuntu 20.04 <sky707070.cv07@nctu.edu.tw>
2022-06-15 13:37:50 +0800

    Add api for save sending message from frontent to database

--- server/routes/message.js
+++ server/routes/message.js
@@ -48,6 +48,47 @@ router.post("/userinfo", async(req, res)=>{
     res.send(resUserInfo)
 })
 
+router.post("/sendmessage", async(req, res)=>{
+    console.log("Hi this is message/sendmessage")
+    
+    const sender_ID = req.body.sender_ID
+    const receiver_ID = req.body.receiver_ID
+
+    // save message to database
+    await new Message({
+        message: req.body.message,
+        sender_ID: sender_ID,
+        receiver_ID: receiver_ID,
+    })
+        .save()
+        .then((NewMessage)=>{
+        console.log("New Message Created.")
+    })
+
+    Message
+        .find({$or:[{'sender_ID':sender_ID}, {'receiver_ID': sender_ID}]}).
+        sort({date: "desc"}).
+        exec((err, messages) => {
+        if(!err) {
+                res.send(messages)
+            }
+        })
+})
+
+/* 
+new User({
+    name: profile.displayName,
+    ID: profile.id,
+    url: String(profile.photos[0].value),
+    email: profile.emails[0].value,
+})
+    .save()
+    .then((NewUser)=>{
+        console.log("New User Created.")
+        done(null, NewUser)
+    })
+*/
+
 // new Message({
 //     sender_ID: req.body.userid,
 //     receiver_ID: 123,
